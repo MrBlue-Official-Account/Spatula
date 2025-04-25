3b138c5b473b3470660137b12d0f45379fdc1d1e
Mr. Blue <GPTl_belike@tuta.io>
2025-04-25 15:26:20 -0400

    Add files via upload

---
+++ Modules/banner.py
@@ -0,0 +1,20 @@
+"""
+    # Banner
+"""
+from colorama import Fore, Style
+
+BRIGHT = Style.BRIGHT
+RED = Fore.RED
+BLUE = Fore.BLUE
+CYAN = Fore.CYAN
+WHITE = Fore.WHITE
+GREEN = Fore.GREEN
+YELLOW = Fore.YELLOW
+MAGENTA = Fore.MAGENTA
+RESET = Fore.RESET
+
+BANNER = f"""\t      {BRIGHT}{BLUE}_____{RESET}
+        {BRIGHT}{BLUE}╔═╗┌─┐|\\|/|┬ ┬┬  ┌─┐{RESET}
+        {BRIGHT}{WHITE}╚═╗├─┘ |@| │ ││  ├─┤
+        ╚═╝┴   |_| └─┘┴─┘┴ ┴
+            {GREEN}\\ v.1.0.0 /{RESET}"""
